10027d02a3ca4b809ddd0745414025653fffe7da
jkemigisha <kjoseph1232018@gmail.com>
2025-11-26 20:49:15 +0300

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -10,8 +10,9 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   
   },
-  base:"/attendance/",
+  
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base:"/attendance/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
